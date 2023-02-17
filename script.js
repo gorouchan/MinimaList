40877bc94d660e382eab8a988104b2af4bc1d1e8
gorouchan <64213053+gorouchan@users.noreply.github.com>
2023-02-17 20:11:39 +0900

    removed unnecessary files, chrome store ready

--- script.js
+++ script.js
@@ -71,8 +71,8 @@ const addTask = function () {
   setTimeout(() => {
     taskList.insertAdjacentHTML(
       'afterbegin',
-      `<div class='task--item '><p class="todo--item">${inputText}</p><button class="todo--btn"><svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 24 24" fill="none" stroke="${
-        localStorage.getItem('dark-mode') === 'true' ? 'black' : 'white'
+      `<div class='task--item '><p class="todo--item">${inputText}</p><button class="todo--btn"><svg xmlns="http://www.w3.org/2000/svg" width="1.3rem" height="1.3rem" viewBox="0 0 24 24" fill="none" stroke="${
+        localStorage.getItem('light-mode') === 'true' ? 'white' : 'black'
       }" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></button></div>`
     );
     taskCount();
@@ -115,8 +115,8 @@ const showTodo = function () {
   todoList.forEach(todo => {
     taskList.insertAdjacentHTML(
       'afterbegin',
-      `<div class='task--item ' ><p class="todo--item">${todo}</p><button class="todo--btn"><svg xmlns="http://www.w3.org/2000/svg" width="1rem" height="1rem" viewBox="0 0 24 24" fill="none" stroke="${
-        localStorage.getItem('dark-mode') === 'true' ? 'black' : 'white'
+      `<div class='task--item ' ><p class="todo--item">${todo}</p><button class="todo--btn"><svg xmlns="http://www.w3.org/2000/svg" width="1.3rem" height="1.3rem" viewBox="0 0 24 24" fill="none" stroke="${
+        localStorage.getItem('light-mode') === 'true' ? 'white' : 'black'
       }" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-trash-2"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path><line x1="10" y1="11" x2="10" y2="17"></line><line x1="14" y1="11" x2="14" y2="17"></line></svg></button></div>`
     );
   });
